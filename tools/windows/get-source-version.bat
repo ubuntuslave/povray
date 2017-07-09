8be3fa00a433fcfcd20316a1a98835556b602a86
Christoph Lipka <c-lipka@users.noreply.github.com>
2017-07-09 22:04:42 +0200

    Modify our Doxygen runner scripts to no longer rely on `unix/VERSION`.
    - `tools/doxygen/doxygen.bat` now makes use of a new PowerShell script and wrapper batch file located in `tools/windows/`, intended as a standard tool to extract version information for use in PowerShell scripts or batch files.
    - `tools/doxygen/doxygen.sh` now relies on `unix/prebuild.sh` already having extracted the version string into `./VERSION` earlier.

---
+++ tools/windows/get-source-version.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM * Extract POV-Ray version information from `source/base/version.h`
+REM *
+REM * Run as follows:
+REM *
+REM *   call "tools/windows/get-source-version.bat" "source/base/version.h"
+REM *
+REM * This will cause the followng envionment variables to be set:
+REM *
+REM *   POV_SOURCE_GENERATION   First two fields of the version string (`X.Y`)
+REM *   POV_SOURCE_VERSION      Full version string (`X.Y.Z`[`.P`][`-PRE`])
+
+powershell -executionpolicy remotesigned -File "%~dp0get-source-version.ps1" "%~1" -bat version~.bat
+call version~.bat
+del version~.bat
